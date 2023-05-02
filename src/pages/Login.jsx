b0085c6c59f92d88da49b328ab701a311e71369d
Francisco Zuidwijk <electrocisko@gmail.com>
2023-05-02 07:36:28 -0300

    Firebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 const Login = () => {
-    return "Login";
+
+  const [email, setEmail ] = useState("");
+  const [password, setPassword ] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("first");
+  };
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="ingrese su mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="ingrese password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
